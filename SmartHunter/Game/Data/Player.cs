97952a80c28d6458fca6c3bd06995741ce9de2b2
khill25 <shadowprincess25@gmail.com>
2020-04-16 15:47:40 -0700

    Added damage caluclations for avg damage per hit and avg damage over the last 15 seconds. No fancy toggles and the code to calculate is called when the player damage is set (used as a metric for tracking damage 'ticks')

--- SmartHunter/Game/Data/Player.cs
+++ SmartHunter/Game/Data/Player.cs
@@ -23,7 +23,10 @@ namespace SmartHunter.Game.Data
         public int Damage
         {
             get { return m_Damage; }
-            set { SetProperty(ref m_Damage, value); }
+            set {
+                SetProperty(ref m_Damage, value);
+                CalculateAndUpdateDPS();
+            }
         }
 
         float m_DamageFraction;
@@ -38,6 +41,120 @@ namespace SmartHunter.Game.Data
         {
             get { return m_BarFraction; }
             set { SetProperty(ref m_BarFraction, value); }
+        }
+
+        // Variables to calculate and store DPS
+        long m_LastTick;
+        float m_DamageAtLastTick;
+        float m_AccumulatedDamage;
+        long m_AccumulatedTime;
+
+        float[] rollingDPSWindow = new float[15];
+        long[] rollingDPSTimes = new long[15]; // parallel array to above
+        long nextWindowIndex = 0;
+
+        float m_TrueDPS;
+        public int TrueDPS
+        {
+            get { return (int)m_TrueDPS; }
+            set { SetProperty(ref m_TrueDPS, value); }
+        }
+
+        float m_MostRecentDPS;
+        public int MostRecentDPS
+        {
+            get { return (int)m_MostRecentDPS; }
+            set { SetProperty(ref m_MostRecentDPS, value); }
+        }
+
+        float m_AvgDamagePerHit;
+        public int AvgDamagePerHit
+        {
+            get { return (int)m_AvgDamagePerHit; }
+            set { SetProperty(ref m_AvgDamagePerHit, value); }
+        }
+
+        void CalculateAndUpdateDPS()
+        {
+            long currentTime = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            long timeDiff = currentTime - m_LastTick;
+            m_LastTick = currentTime;
+
+            var damageSinceLastTick = m_Damage - m_DamageAtLastTick;
+            m_DamageAtLastTick = m_Damage; // update the last damage
+            m_AccumulatedDamage += damageSinceLastTick;
+            m_AccumulatedTime += timeDiff; // track damage time events
+            TrueDPS = (int)(m_AccumulatedDamage / m_AccumulatedTime);
+            if (damageSinceLastTick <= 0.01)
+            { return; }
+            var damageDoneInTimeInterval = damageSinceLastTick / timeDiff;
+
+            // Rolling window damage calculation and logic
+            rollingDPSWindow[nextWindowIndex] = damageSinceLastTick;
+            rollingDPSTimes[nextWindowIndex] = currentTime;
+            nextWindowIndex += 1;
+            if (nextWindowIndex >= 15)
+            {
+                nextWindowIndex = 0;
+            }
+
+            // calculate avg dmage per hit
+            AvgDamagePerHit = (int)Average(rollingDPSWindow);
+
+            // Calculate the rolling dps
+            MostRecentDPS = (int)Average(rollingDPSWindow, rollingDPSTimes);
+
+        }
+
+        private float Average(float[] values)
+        {
+
+            if (values.Length == 0)
+            {
+                return 0;
+            }
+
+            float totalValue = 0;
+            int numValuesUsed = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] <= 0.01)
+                { continue; }
+                totalValue += values[i];
+                numValuesUsed++;
+            }
+
+            return totalValue / numValuesUsed;
+        }
+
+        private float Average(float[] values, long[] times) {
+            if (values.Length == 0)
+            {
+                return 0;
+            }
+
+            float totalValue = 0;
+            long totalTime = 0;
+            long lowestTime = long.MaxValue;
+            int numValuesUsed = 0;
+            long now = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            for (int i = 0; i < values.Length; i++)
+            {
+                // We don't care if the time is 0
+                if (times[i] == 0 || times[i] - now > 15000) // also ignore entries more than 15 seconds ago. Only include the most recent 15 seconds
+                {
+                    continue;
+                }
+                numValuesUsed++;
+                totalValue += values[i];
+                totalTime += times[i];
+                if (times[i] < lowestTime) {
+                    lowestTime = times[i];
+                }
+            }
+
+            long adjustedTime = ((totalTime/numValuesUsed) - lowestTime)/1000; // subtract the lowest time in the array because we want relative time -- that is the total time that has elapsed in the window captured
+            return totalValue / adjustedTime;
         }
     }
 }
